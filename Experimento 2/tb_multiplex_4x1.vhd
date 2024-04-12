ec632b630ab2275dd36e4b3df02e20a8594c0262
Gabriel Queiroz <gabrielr.queiroz@gmail.com>
2024-04-12 10:22:16 -0300

    Projeto 2: Detalhamento do Testbench

--- Experimento 2/tb_multiplex_4x1.vhd
+++ Experimento 2/tb_multiplex_4x1.vhd
@@ -1,14 +1,14 @@
--- Universidade de BrasÌ≠lia
--- LaboratÛrio de Sistemas Digitais
+-- Universidade de BrasÔøΩÔøΩlia
+-- LaboratÔøΩrio de Sistemas Digitais
 -- Autor: Gabriel Roberto de Queiroz
 -- Data: 12/04/2024
 
 -- ************
--- Testbench para a simulaÁ„o do
--- Circuito: Multiplexador 4x1 com 4 bits de dados, 2 bits de seleÁ„o e uma saÌ≠da:
+-- Testbench para a simulaÔøΩÔøΩo do
+-- Circuito: Multiplexador 4x1 com 4 bits de dados, 2 bits de seleÔøΩÔøΩo e uma saÔøΩÔøΩda:
 --          data   Entrada de 4 bits de dados
---          sel   Entrada de 2 bits de seleÁ„o
---          y   SaÌ≠da do mux
+--          sel   Entrada de 2 bits de seleÔøΩÔøΩo
+--          y   SaÔøΩÔøΩda do mux
 -- ************
 
 -- ************ Package ************
@@ -16,92 +16,54 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 USE std.textio.ALL;
+USE IEEE.numeric_std.ALL;
 
 -- ************ Entity ************
 -- testbench: uma entidade sem pinos de entrada e sa√≠da
 
 -- Testbench para multiplex_4x1.vhd
--- ValidaÁ„o assÌncrona
+-- ValidaÔøΩÔøΩo assÔøΩncrona
 ENTITY multiplex_testbench IS END;
 
 -- ************ Architecture ************
--- implementaÁ„o do projeto
+-- implementaÔøΩÔøΩo do projeto
 
 ARCHITECTURE tb_multiplex_4x1 OF multiplex_testbench IS
-   -- declaraÁ„o do componente multiplex_4x1 de acordo com sua arquitetura no arquivo multiplex_4x1.vhd
+   -- declaraÔøΩÔøΩo do componente multiplex_4x1 de acordo com sua arquitetura no arquivo multiplex_4x1.vhd
    COMPONENT multiplex_4x1
       PORT (
-         -- declaraÁ„o dos pinos de entrada
+         -- declaraÔøΩÔøΩo dos pinos de entrada
          data : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
          sel  : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-         -- declaraÁ„o dos pinos de sa√≠da
+         -- declaraÔøΩÔøΩo dos pinos de sa√≠da
          y : OUT STD_LOGIC
       );
    END COMPONENT;
 
-   -- Sinais auxiliares para a simulaÁ„o dos estÌ≠mulos ao circuito
+   -- Sinais auxiliares para a simulaÔøΩÔøΩo dos estÔøΩÔøΩmulos ao circuito
    SIGNAL data_aux : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0000";
    SIGNAL sel_aux  : STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";
 
-   -- Inst‚ncia do componente multiplex_4x1 e conex„o dos sinais
+   -- InstÔøΩncia do componente multiplex_4x1 e conexÔøΩo dos sinais
 BEGIN
    multiplex : multiplex_4x1 PORT MAP(
-      -- conex„o dos pinos de entrada
+      -- conexÔøΩo dos pinos de entrada
       data => data_aux,
       sel  => sel_aux,
-      -- conex„o dos pinos de saÌda
+      -- conexÔøΩo dos pinos de saÔøΩda
       y => OPEN
    );
 
-   -- Processo para gerar os est√≠Ìulos
+   -- Processo para gerar os est√≠ÔøΩulos
    estimulo : PROCESS
    BEGIN
-      WAIT FOR 5 ns;
-      sel_aux <= "01";
-      WAIT FOR 5 ns;
-      sel_aux <= "10";
-      WAIT FOR 5 ns;
-      sel_aux <= "11";
-      WAIT FOR 5 ns;
-
-      data_aux <= "1000";
-      sel_aux  <= "00";
-      WAIT FOR 5 ns;
-      sel_aux <= "01";
-      WAIT FOR 5 ns;
-      sel_aux <= "10";
-      WAIT FOR 5 ns;
-      sel_aux <= "11";
-      WAIT FOR 5 ns;
-
-      data_aux <= "0100";
-      sel_aux  <= "00";
-      WAIT FOR 5 ns;
-      sel_aux <= "01";
-      WAIT FOR 5 ns;
-      sel_aux <= "10";
-      WAIT FOR 5 ns;
-      sel_aux <= "11";
-      WAIT FOR 5 ns;
-
-      data_aux <= "0010";
-      sel_aux  <= "00";
-      WAIT FOR 5 ns;
-      sel_aux <= "01";
-      WAIT FOR 5 ns;
-      sel_aux <= "10";
-      WAIT FOR 5 ns;
-      sel_aux <= "11";
-      WAIT FOR 5 ns;
-
-      data_aux <= "0001";
-      sel_aux  <= "00";
-      WAIT FOR 5 ns;
-      sel_aux <= "01";
-      WAIT FOR 5 ns;
-      sel_aux <= "10";
-      WAIT FOR 5 ns;
-      sel_aux <= "11";
+      for i in 0 to 15 loop
+         for j in 0 to 3 loop
+            data_aux <= std_logic_vector(to_unsigned(i, 4));
+            sel_aux  <= std_logic_vector(to_unsigned(j, 2));
+            WAIT FOR 10 ns;
+         end loop;
+      end loop;
 
       WAIT;
    END PROCESS estimulo;
